cbc76923908b1a1bcb4fdcf594ffea820224c275
Matt Adams <matt@forestgiant.com>
2018-11-16 10:04:16 -0500

    On-screen renaming implemented.
    - Need to maintain a separate vector for holding renamed files, then
    compare against this as we perform the actual rename.
    - Need to handle backspaces
    - Cursor should blink
    - Files without extensions need to be handled properly

--- srnm.cpp
+++ srnm.cpp
@@ -45,6 +45,8 @@ void printDirectory(std::vector<std::string> files, WINDOW *filesWin)
                  winWidth = 0;
 
   getmaxyx(stdscr, winHeight, winWidth);
+  wclear(filesWin);
+  box(filesWin, 0, 0);
 
   for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); it++)
     {
@@ -59,33 +61,85 @@ void printDirectory(std::vector<std::string> files, WINDOW *filesWin)
       filesY++;
       filesIdx++;
     }
+
+  update_panels();
+  doupdate();
 }
+
+void handleRename(std::string newFilename, std::vector<std::string> files, WINDOW *filesWin)
+{
+  unsigned short numFiles = files.size(),
+                 numLeadingZeroes = 1, 
+                 filesIdx = 0;
+
+  while ((numFiles = numFiles / 10) > 0)
+  {
+    numLeadingZeroes++;
+  }
+  
+  for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); it++)
+  {
+    std::string period = ".";
+    std::string dash = "-";
+    std::string extension = period.append((*it).substr((*it).find_last_of(".") + 1));
+    std::string fileNumber = dash.append(std::to_string(filesIdx).append(extension));
+    (*it) = newFilename;
+    (*it) = (*it).append(fileNumber);
+    filesIdx++;
+  }
+
+  
+  printDirectory(files, filesWin);
+}
+
+
 
 int main(int argc, char* argv[])
 {
   WINDOW *filesWin;
   PANEL *filesPanel;
+  cbreak();
 
   unsigned short filesPanelY = 3,
                  filesPanelX = 0,
                  termHeight = 0,
                  termWidth = 0;
 
+  char ch;
+
   initscr();
   getmaxyx(stdscr, termHeight, termWidth);
   filesWin = newwin(termHeight - filesPanelY, termWidth, filesPanelY, filesPanelX);
-  box(filesWin, 0, 0);
 
   std::vector<std::string> files = getFiles();
-
-  printDirectory(files, filesWin);
+  std::string newFilename;
   
-  mvprintw(0, 0, "Enter template filename: ");
+  start_color();
+  init_pair(1, COLOR_RED, COLOR_BLACK);
+ 
   filesPanel = new_panel(filesWin);
-  update_panels();
-  doupdate();
-  getch();
-  endwin();
+  printDirectory(files, filesWin);
 
-  return(0);
+  attron(A_BOLD);
+  attron(COLOR_PAIR(1));
+
+  std::string instructions = "(<ENTER> to rename, <CTRL+C> to quit)";
+  mvprintw(0, (termWidth - instructions.length()), instructions.c_str());
+
+  attroff(A_BOLD);
+  attroff(COLOR_PAIR(1));
+
+  mvprintw(0, 0, "Enter template filename: ");
+   
+  attron(A_BOLD);
+  attron(COLOR_PAIR(1));
+  
+  while ((ch = getch()) != KEY_F(1))
+  {
+    newFilename += ch;
+    handleRename(newFilename, files, filesWin);
+  }
+
+  endwin();
+  return 0;
 }
