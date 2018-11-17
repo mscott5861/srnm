2518e74d0f891d72dacac1f2de0c455d8c0ce9ff
Matt Adams <matt@forestgiant.com>
2018-11-17 11:25:52 -0500

    Implemented file renaming, warning/confirmation, and placed limits on strings.

---
+++ README.md
@@ -0,0 +1,9 @@
+## srnm
+
+A deliberately simple utility that sequentially (and dumbly) renames the files in the directly it's invoked from to whichever string you enter. Useful for bulk renaming things like wallpapers, photos, etc.
+
+## Usage
+
+`cd` to the directory whose files you'd like to rename, and just run `srnm`. Type in the new filename, press `Enter` to rename the files (followed by a confirmation), or `Ctrl+C` to exit the program.
+
+## Installation
--- srnm.cpp
+++ srnm.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <dirent.h>
 #include <curses.h>
 #include <panel.h>
@@ -6,26 +6,23 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <iomanip>
 
 std::vector<std::string> getFiles() 
 {
   DIR *directory;
   struct dirent *dir;
-  unsigned short numFiles = 0;
   std::vector<std::string> files;
 
   directory = opendir(".");
 
   if (directory)
   {
-    while ((dir = readdir(directory)) != NULL && numFiles < 30) 
+    while ((dir = readdir(directory)) != NULL) 
     {
       std::string filename = dir->d_name;
 
       if (filename.compare(".") != 0 && filename.compare("..") != 0) {
         files.push_back(filename);
-        numFiles++;
       }
     }
 
@@ -35,8 +32,34 @@ std::vector<std::string> getFiles()
   return files;
 }
 
+void writeRenameToDisk(std::vector<std::string> newFiles, std::vector<std::string> files)
+{
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  unsigned short idx = 0;
 
-void printDirectory(std::vector<std::string> files, WINDOW *filesWin)
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL) 
+    {
+      std::string filename = dir->d_name;
+
+      if (filename.compare(".") != 0 && filename.compare("..") != 0) 
+      {
+        if (files[idx].compare(filename) == 0)
+        {
+          rename(filename.c_str(), newFiles[idx].c_str());
+          idx++;
+        }
+      }
+    }
+  }
+
+  closedir(directory);
+}
+
+void printDirectory(std::vector<std::string> *files, WINDOW *filesWin)
 {
   unsigned short padding = 2,
                  filesIdx = 0,
@@ -49,7 +72,8 @@ void printDirectory(std::vector<std::string> files, WINDOW *filesWin)
   werase(filesWin);
   box(filesWin, 0, 0);
 
-  for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); it++)
+  wattron(filesWin, A_BOLD | COLOR_PAIR(2));
+  for (std::vector<std::string>::iterator it = (*files).begin(); it != (*files).end(); it++)
     {
       if (filesIdx > 0 && filesIdx % (winHeight - 7) == 0)
       {
@@ -65,66 +89,99 @@ void printDirectory(std::vector<std::string> files, WINDOW *filesWin)
 
   update_panels();
   doupdate();
+  wattroff(filesWin, A_BOLD | COLOR_PAIR(2));
 }
 
-void handleAlphanumericKeypress(std::string newFilename, std::vector<std::string> newFiles, std::vector<std::string> files, WINDOW *filesWin)
+unsigned short getDigitCount(unsigned short digit)
 {
-  unsigned short numFiles = files.size(),
-                 maxDigitCount = 1, 
-                 filesIdx = 1,
-                 digitIdx = 0,
-                 idx = 0;
+  unsigned short numDigits = 1;
 
-  while ((numFiles = numFiles / 10) > 0)
+  while ((digit = digit / 10) > 0)
   {
-    maxDigitCount++;
+    numDigits++;
   }
-  
-  for (std::vector<std::string>::iterator it = files.begin(); it != files.end(); it++)
+
+  return numDigits;
+}
+
+std::string getLeadingZeroes(unsigned short *maxDigitCount, unsigned short *filesIdx)
+{
+  std::string leadingZeroes;
+  unsigned short idx = 0;
+
+  for (idx = 0; idx < (getDigitCount(*maxDigitCount) - (getDigitCount(*filesIdx) - 1)); idx++)
   {
-    unsigned short numDigits = 0;
-    idx = filesIdx - 1;
+    leadingZeroes.append("0");
+  }
 
-    std::string fileNumber;
-    unsigned short digitIdx = maxDigitCount - numDigits;
+  return leadingZeroes;
+}
 
-    fileNumber = std::setfill('0');
-    fileNumber = std::setw(maxDigitCount);
-    
-    std::string period = ".";
-    std::string dash = "-";
-    std::string extension = period.append((*it).substr((*it).find_last_of(".") + 1));
-    fileNumber = dash.append(fileNumber.append(std::to_string(filesIdx)).append(extension));
+void handleAlphanumericKeypress(std::string newFilename, std::vector<std::string> *newFiles, std::vector<std::string> *files, WINDOW *filesWin)
+{
+  unsigned short numFiles = files->size(),
+                 maxDigitCount = getDigitCount(numFiles), 
+                 filesIdx = 1;
+  
+  for (std::vector<std::string>::iterator it = (*newFiles).begin(); it != (*newFiles).end(); it++)
+  {
+    std::string leadingZeroes = getLeadingZeroes(&maxDigitCount, &filesIdx),
+                period = ".",
+                dash = "-",
+                extension = period.append((*it).substr((*it).find_last_of(".") + 1));
+
+    leadingZeroes = dash.append(leadingZeroes.append(std::to_string(filesIdx)).append(extension));
     (*it) = newFilename;
-    (*it) = (*it).append(fileNumber);
+    (*it) = (*it).append(leadingZeroes);
     filesIdx++;
   }
 
-  printDirectory(files, filesWin);
-  move(0, (25 + newFilename.length()));
-}
-
-void writeRenameToDisk(std::vector<std::string> newFiles, std::vector<std::string> files)
-{
-
+  printDirectory(newFiles, filesWin);
+  move(0, (27 + newFilename.length()));
 }
 
 void printInstructions(unsigned short *termWidth)
 {
-  attron(A_BOLD);
-  attron(COLOR_PAIR(1));
+  attron(A_BOLD | COLOR_PAIR(1));
 
   std::string instructions = "(<ENTER> to rename, <CTRL+C> to quit)";
   mvprintw(0, (*termWidth - instructions.length()), instructions.c_str());
 
-  attroff(A_BOLD);
-  attroff(COLOR_PAIR(1));
+  attroff(A_BOLD | COLOR_PAIR(1));
+}
+
+void printWarning(PANEL *warningPanel, WINDOW *warningWin)
+{
+  unsigned short boxHeight = 0,
+                 boxWidth = 0;
+
+  warningPanel = new_panel(warningWin);
+  werase(warningWin);
+  box(warningWin, 0, 0);
+
+  std::string warning = "This action cannot be undone.",
+              confirm = "Press <ENTER> once more to confirm.",
+              cancel = "Press <CTRL+C> to cancel and quit.";
+
+  getmaxyx(warningWin, boxHeight, boxWidth);
+
+  wattron(warningWin, A_BOLD | COLOR_PAIR(1));
+
+  mvwprintw(warningWin, (boxHeight / 2) - 1, (boxWidth / 2) - (warning.length() / 2), warning.c_str());
+  mvwprintw(warningWin, (boxHeight / 2), (boxWidth / 2) - (confirm.length() / 2), confirm.c_str());
+  mvwprintw(warningWin, (boxHeight / 2) + 1, (boxWidth / 2) - (cancel.length() / 2), cancel.c_str());
+
+  wattroff(warningWin, A_BOLD | COLOR_PAIR(1));
+
+  move(0,0);
+  update_panels();
+  doupdate();
 }
 
 int main(int argc, char* argv[])
 {
-  WINDOW *filesWin;
-  PANEL *filesPanel;
+  WINDOW *filesWin, *warningWin;
+  PANEL *filesPanel, *warningPanel;
   cbreak();
 
   unsigned short filesPanelY = 3,
@@ -133,26 +190,28 @@ int main(int argc, char* argv[])
                  termWidth = 0;
 
   int ch;
+  bool hasAccepted = false;
 
   initscr();
   getmaxyx(stdscr, termHeight, termWidth);
   filesWin = newwin(termHeight - filesPanelY, termWidth, filesPanelY, filesPanelX);
+  warningWin = newwin(termHeight / 2, termWidth / 2, (termHeight / 2) - (termHeight / 4), (termWidth / 2) - (termWidth / 4));
 
-  std::vector<std::string> files = getFiles();
-  std::vector<std::string> newFiles;
+  std::vector<std::string> files = getFiles(),
+                           newFiles = files;
   std::string newFilename;
   
   start_color();
   init_pair(1, COLOR_RED, COLOR_BLACK);
+  init_pair(2, COLOR_GREEN, COLOR_BLACK);
  
   filesPanel = new_panel(filesWin);
-  printDirectory(files, filesWin);
+  printDirectory(&files, filesWin);
   printInstructions(&termWidth);
 
-  mvprintw(0, 0, "Enter template filename: ");
+  mvprintw(0, 0, "Enter sequential filename: ");
    
-  attron(A_BOLD);
-  attron(COLOR_PAIR(1));
+  attron(A_BOLD | COLOR_PAIR(1));
 
   noecho();
   
@@ -161,31 +220,39 @@ int main(int argc, char* argv[])
     noecho();
     switch(ch)
     {
+      // KEY_BACKSPACE isn't currently working, so we'll use 127 instead.
       case 127:
         if (newFilename.length() > 0) {
           newFilename.pop_back();
 
-          move(0, 25);
+          move(0, 27);
           clrtoeol();
        
-          mvprintw(0, 25, newFilename.c_str());
+          mvprintw(0, 27, newFilename.c_str());
           printInstructions(&termWidth);
-          attron(A_BOLD);
-          attron(COLOR_PAIR(1));
+          attron(A_BOLD | COLOR_PAIR(1));
 
-          handleAlphanumericKeypress(newFilename, newFiles, files, filesWin);
+          handleAlphanumericKeypress(newFilename, &newFiles, &files, filesWin);
         }
         break;
 
       case 10:
-        writeRenameToDisk(newFiles, files);
-        endwin();
-        return 0;
+        if (!hasAccepted) {
+          printWarning(warningPanel, warningWin);
+          hasAccepted = true;
+        } else {
+          writeRenameToDisk(newFiles, files);
+          endwin();
+          return 0;
+        }
+        break;
 
       default:
-        newFilename += ch;
-        mvprintw(0, 25, newFilename.c_str());
-        handleAlphanumericKeypress(newFilename, newFiles, files, filesWin);
+        if (!hasAccepted && newFilename.length() < 10) {
+          newFilename += ch;
+          mvprintw(0, 27, newFilename.c_str());
+          handleAlphanumericKeypress(newFilename, &newFiles, &files, filesWin);
+        }
     }
   }
 
